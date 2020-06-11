0297f5c31034b0543fafdef14c33bd1371085ac1
uday dosajh <dosajhuday@gmail.com>
2020-06-11 23:39:40 +0530

    packaging module

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
-     name='ud_util',  
-     version='0.1',
-     author="Uday Dosajh",
-     author_email="dosajhuday@gmail.com",
-     description="sqlite utility",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     packages=find_packages()
- )
+    name='ud_util',
+    version='0.1',
+    author="Uday Dosajh",
+    author_email="dosajhuday@gmail.com",
+    description="sqlite utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages()
+)
+print(str(find_packages()))
+print("process completed")
